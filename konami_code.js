a29728385cc0f4ae226c1cb45b75778814510da0
Dale Sublett <djsublett04@gmail.com>
2019-04-01 19:14:37 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,32 +1,33 @@
-  function triggerKeyDown(key) {
-    const keyboardEvent = new KeyboardEvent("keydown", { key });
-    document.body.dispatchEvent(keyboardEvent);
+const codes = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
+];
+
+let index = 0;
+
+document.body.addEventListener("keydown", (thingPressed) => {
+  const key = thingPressed.key 
+  console.log(key) 
+  
+  if (codes[index] == key) {
+    ++index 
+  } 
+  
+  else {
+    idx = 0 
   }
   
-  init()
-
-  describe("Konami code", () => {
-    
-    const spy = sinon.stub(window, "alert");
-    
-    it("triggers an alert if the right code is entered", () => {
-      
-      for (let i = 0, l = codes.length; i < l; i++) {
-        triggerKeyDown(codes[i]);
-      }
-      expect(spy.called).to.equal(true)
-      expect(spy.callCount).to.equal(1)
-    });
-
-    it("does not trigger an alert if the wrong code is entered", () => {
-      spy.reset()
-
-      for (let i = 0; i < codes.length - 1; i++) {
-        triggerKeyDown(codes[i])
-      }
-      triggerKeyDown("ArrowUp")
-      
-      expect(spy.notCalled).to.equal(true);
-    });
-  });
-});
+  console.log(index)
+  if (index == 10) {
+    alert("Hurray!");
+    index = 0 
+  }
+}); 
