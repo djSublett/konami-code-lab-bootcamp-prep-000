56d5f756190b06788689ecf683263986abec78dd
Dale Sublett <djsublett04@gmail.com>
2019-04-01 19:11:02 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,18 +1,20 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-let index = 0;
-function init() {
-  documnt.body.addEventListener("keydown", function(event){
-    const key=parseInt(event.which);
-    console.log(event.which);
-    if(key===code[index]){
-      index++;
-      if(index===code.length){
-        window.alert("Cheat activated, you cheater")
-      }
-    }
-    else{
-      index=0;
-    }
-  })
-}
 init();
+
+    function init() {
+
+        var index = 0;
+        const getElement = document.querySelector('body');
+
+        getElement.addEventListener('keydown', function(e) {
+            if (code[index] === e.which || code[index] === e.detail) {
+                index++;
+
+                if (index === code.length) {
+                    alert("CHEAT EXECUTED!!");
+                    index = 0;
+                }
+            } else {
+                index = 0;
+            }
+        })
+    }
